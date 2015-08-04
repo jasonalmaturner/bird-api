45abfbdd607243d98fa44e68c726c1030bedabf9
Jason Turner <jasonalmaturner@gmail.com>
2015-08-03 22:23:59 -0600

    seeded my db w/ bird data

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var express = require('express'),
   cors = require('cors'),
   mongoose = require('mongoose'),
   mongoUri = process.env.MONGOURI || 'mongodb://localhost:27017/sample',
-  port = process.env.EXPRESS_PORT || 9001;
+  port = process.env.EXPRESS_PORT || 9001,
+  seeder = require('mongoose-seed')
+  birds = require('./seeds/birds.json');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -16,4 +18,18 @@ app.listen(port, function(){
 mongoose.connect(mongoUri);
 mongoose.connection.once('open', function(){
   console.log('db connected at:', mongoUri);
-})
+});
+
+// seeder.connect(mongoUri, function(){
+//   console.log(1111)
+//   seeder.loadModels(['./models/birdModel']);
+//   console.log(22222)
+//   seeder.clearModels(['Birds'], function(){
+//     console.log(3333)
+//     seeder.populateModels([{
+//       'model': 'Birds',
+//       'documents': birds
+//     }]);
+//     console.log(4444)
+//   });
+// });
---
+++ models/birdModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var birdSchema = new mongoose.Schema({
+  order: { type: String, lowercase: true, index: true},
+  familyName: { type: String, lowercase: true, index: true},
+  family: { type: String, lowercase: true, index: true},
+  commonName: { type: String, lowercase: true, index: true},
+  scientificName: { type: String, lowercase: true, index: true},
+  authority: { type: String, lowercase: true},
+  BirdLifeTaxonomicTreatment: { type: String, lowercase: true},
+  IUCNRedListCategory2014: { type: String, lowercase: true},
+  synonyms: { type: String, lowercase: true},
+  alternativeCommonNames: { type: String, lowercase: true, index: true},
+  taxonomicNotes: { type: String, lowercase: true},
+  taxonomicSources: { type: String, lowercase: true},
+  SISRecID: { type: Number, index: true },
+  SpcRecID: { type: Number, index: true }
+});
+
+module.exports = mongoose.model('Birds', birdSchema);
