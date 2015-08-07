2732552ff7417bc3596bda0aca5e68c3006d4205
Jason Turner <jasonalmaturner@gmail.com>
2015-08-06 22:35:31 -0600

    changing schemas indexes

--- models/birdModel.js
+++ models/birdModel.js
@@ -5,7 +5,7 @@ var birdSchema = new mongoose.Schema({
   familyName: { type: String, lowercase: true, index: true, require: true },
   family: { type: String, lowercase: true, index: true},
   commonName: { type: String, lowercase: true, index: true, require: true },
-  scientificName: { type: String, index: true, require: true },
+  scientificName: { type: String, sparse: true, unique: true, require: true },
   authority: { type: String, lowercase: true},
   BirdLifeTaxonomicTreatment: { type: String, lowercase: true, enum: ['r', 'nr', 'ur']},
   IUCNRedListCategory2014: { type: String, lowercase: true, enum: ["lc", "nt", "vu", "en", "cr", "cr (pe)", "cr (pew)", "ew", "ex", "dd", "nr", "ur"] },
@@ -13,8 +13,8 @@ var birdSchema = new mongoose.Schema({
   alternativeCommonNames: { type: String, lowercase: true, index: true},
   taxonomicNotes: { type: String, lowercase: true},
   taxonomicSources: { type: String, lowercase: true},
-  SISRecID: { type: Number, index: true },
-  SpcRecID: { type: Number, index: true }
+  SISRecID: { type: Number, sparse: true, unique: true },
+  SpcRecID: { type: Number, sparse: true, unique: true }
 });
 
 module.exports = mongoose.model('Birds', birdSchema);
