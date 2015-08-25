3adbf2e4a59356dfd07f97c4dd0f9e17655f3b36
Jason Turner <jasonalmaturner@gmail.com>
2015-08-25 10:37:46 -0600

    repopulate data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
   cors = require('cors'),
   mongoose = require('mongoose'),
   mongoUri = process.env.MONGOURI || 'mongodb://localhost:27017/sample',
-  port = process.env.EXPRESS_PORT || 80,
+  port = process.env.EXPRESS_PORT || 9001,
   seeder = require('mongoose-seed'),
   birds = require('./seeds/birds.json'),
   birdCtrl = require('./controllers/birdController'),
@@ -30,22 +30,8 @@ app.delete('/api/birds/:id', birdCtrl.removeBird);
 // });
 
 seeder.connect(mongoUri, function(){
-  // console.log('db connected at', mongoUri);
-  // console.log('cron job running', counter++);
-  // seeder.loadModels(['./models/birdModel']);
-  // console.log(111)
-  // seeder.clearModels(['Birds'], function(){
-  //   console.log(222)
-  //   seeder.populateModels([{
-  //     'model': 'Birds',
-  //     'documents': birds
-  //   }]);
-  //   console.log(333);
-  // });
-});
-
-var job = new CronJob('00 50 12 * * 0-6', function(){
-  console.log('cron job running', counter++)
+  console.log('db connected at', mongoUri);
+  console.log('cron job running', counter++);
   seeder.loadModels(['./models/birdModel']);
   console.log(111)
   seeder.clearModels(['Birds'], function(){
@@ -54,8 +40,22 @@ var job = new CronJob('00 50 12 * * 0-6', function(){
       'model': 'Birds',
       'documents': birds
     }]);
-    console.log(333)
+    console.log(333);
   });
-}, function(){
-  console.log('db reset');
-}, true);
+});
+//
+// var job = new CronJob('00 50 12 * * 0-6', function(){
+//   console.log('cron job running', counter++)
+//   seeder.loadModels(['./models/birdModel']);
+//   console.log(111)
+//   seeder.clearModels(['Birds'], function(){
+//     console.log(222)
+//     seeder.populateModels([{
+//       'model': 'Birds',
+//       'documents': birds
+//     }]);
+//     console.log(333)
+//   });
+// }, function(){
+//   console.log('db reset');
+// }, true);
